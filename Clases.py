2c744a097b042659289276a5f2b3c3f3fba6d9f0
Gonza-e <Gonzaloescobar0904@gmail.com>
2025-10-17 18:42:01 -0300

    Clases Planificador y Memoria agregadas

--- Clases.py
+++ Clases.py
@@ -1,4 +1,8 @@
-ESTADO_EJECUCION = 'running'
+LISTO = 'Ready'
+SUSPENDIDO = 'Suspended'
+EJECUCION = 'Running'
+NUEVO = 'New'
+TERMINADO = 'Exit'
 
 #Clase para las particiones
 class Particion:
@@ -14,7 +18,7 @@ class Proceso:
     def __init__(self,id,tam,ti,ta):
         self.id = id 
         self.tamaño = tam 
-        self.estado = '-'
+        self.estado = NUEVO
         self.t_arribo = ta
         self.t_irrupcion = ti
         self.t_irrupcion_faltante = ti 
@@ -24,6 +28,57 @@ class Proceso:
 
 #Clase para la CPU       
 class CPU:
-    def __init__(self,proceso):
+    def __init__(self,proceso=None):
         self.proceso = None 
+
+#Clase para la memoria
+class Memoria: 
+    def __init__(self):
+        self.particiones = [ 
+            Particion(1,100,0,"SO"),        #Particion para el sistema operativo
+            Particion(1,250,100,None),
+            Particion(2,150,350,None),
+            Particion(3,50,500,None)
+        ]
+
+    #Asignacion Best-fit
+    def asignarProceso(self,proceso):
+        mejor = None        #Inicializamos la variable mejor para la mejor particion
+        for p in self.particiones:       #Recorremos la lista de particiones
+            if p.proceso is None and proceso.tamaño < p.tamaño:         #Si la particion esta vacia y el tamaño del proceso es menor que la particion     
+                if mejor is None or mejor.tamaño < p.tamaño:        #Si mejor esta vacio o el tamaño de la mejor particion actual es menor al tamaño de otra particion 
+                    mejor = p       #Se asigna el proceso a la particion
+
+                if mejor:       #Si mejor no esta vacio entonces se le asigna el proceso a la mejor particion
+                    mejor.proceso = proceso 
+                    mejor.fragmentacion = mejor.tamaño - proceso.tamaño 
+                    proceso.estado = LISTO
+                    return True 
+                else: 
+                    proceso.estado = SUSPENDIDO
+                    return False        #Devuelve falso en caso de no encontrarse una particion 
+    
+    def liberarParticion(self,proceso):         #Libera la particion 
+        for p in self.particiones:
+            if p.proceso == proceso:
+                p.proceso = None 
+                p.fragmentacion = p.tamaño 
+
+#Clase para el Planificador SRTF
+class Planificador:    
+    def __init__(self):
+        self.cola_de_listos = []
+        self.cola_de_suspendidos = []
+        self.tiempo = 0         #Tiempo global
+    
+    def agregarProceso(self,proceso):
+        self.cola_de_listos.append(proceso)
+        self.cola_de_listos.sort(key=lambda p: p.t_irrupcion_faltante)      #Ordena la cola de listos segun el tiempo de irrupcion faltante
+        
+    def siguienteProceso(self,proceso=None):
+        if self.cola_de_listos:
+            return self.cola_de_listos[0]        #Selecciona el primer procesos de la cola de lsitos
+        return None
+    
+
 
